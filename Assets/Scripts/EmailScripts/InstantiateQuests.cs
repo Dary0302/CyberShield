a1fd2dcce0a6603e5f15299e7232e9f662f4e6ff
Dary <Leontyev0302@mail.ru>
2024-06-13 13:00:24 +0500

    update sound-accept-quest.mp3

--- Assets/Scripts/EmailScripts/InstantiateQuests.cs
+++ Assets/Scripts/EmailScripts/InstantiateQuests.cs
@@ -1,28 +1,31 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class InstantiateQuests : MonoBehaviour
+namespace EmailScripts
 {
-    [SerializeField] private LetterView letterViewPrefab;
-    [SerializeField] private Transform content;
-    [SerializeField] private List<SampleQuest> quests = new();
-
-    private void Start()
+    public class InstantiateQuests : MonoBehaviour
     {
-        var countQuests = 0;
-        foreach (var quest in quests)
-        {
-            if (countQuests > PlayerStats.GetLevelsCompletedNumber())
-                break;
-            
-            var newLetter = Instantiate(letterViewPrefab, content.position, Quaternion.identity, content);
-            
-            if (countQuests < PlayerStats.GetLevelsCompletedNumber())
-                newLetter.SetDoneCheckMark();
+        [SerializeField] private LetterView letterViewPrefab;
+        [SerializeField] private Transform content;
+        [SerializeField] private List<SampleQuest> quests = new();
 
-            countQuests++;
+        private void Start()
+        {
+            var countQuests = 0;
+            foreach (var quest in quests)
+            {
+                if (countQuests > PlayerStats.GetLevelsCompletedNumber())
+                    break;
             
-            newLetter.SetData(quest);
+                var newLetter = Instantiate(letterViewPrefab, content.position, Quaternion.identity, content);
+            
+                if (countQuests < PlayerStats.GetLevelsCompletedNumber())
+                    newLetter.SetDoneCheckMark();
+
+                countQuests++;
+            
+                newLetter.SetData(quest);
+            }
         }
     }
 }
